3698178ebeb1ca0c60a2995c418a8695c58c906a
Danielid Ramirez <danielid132@gmail.com>
2021-09-29 19:39:29 -0300

    Add: admin_users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'admin@techstaff.com', password: 'tech1234', password_confirmation: 'tech1234') if Rails.env.development?
