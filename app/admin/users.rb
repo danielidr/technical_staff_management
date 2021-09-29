8f1f407155b6fac39f2ff7b3aa39d4bfff926445
Danielid Ramirez <danielid132@gmail.com>
2021-09-29 20:39:17 -0300

    Add: Custom form to create users

---
+++ app/admin/users.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :email, :password, :name, :role
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name, :role]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  form do |f|
+    inputs 'Agregar un nuevo usuario' do
+      input :email
+      input :password
+      input :name
+      f.input :role,
+        label: 'Role',
+        as: :select,
+        collection: Role.pluck(:name)
+    end
+    actions
+  end
+  
+end
