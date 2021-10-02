7d9d25043186b8fecf94050749ec8b50b6f64b4a
Danielid Ramirez <danielid132@gmail.com>
2021-10-01 23:52:49 -0300

    Add: Order relations to models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,6 @@
 class Client < ApplicationRecord
     belongs_to :address
+    has_many :orders
 
     validates_presence_of :first_name, :last_name, :rut, :email, :phone
     validates :rut, :email, uniqueness: true
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :client
   belongs_to :address
+  belongs_to :user
+  belongs_to :created_by, :class_name => 'User'
 end
