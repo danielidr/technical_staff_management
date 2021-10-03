569998d0d067f451660af27a6e1a3b17edb32646
Danielid Ramirez <danielid132@gmail.com>
2021-10-02 22:36:51 -0300

    Add: order relation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   belongs_to :address, optional: true
   belongs_to :user, optional: true
   belongs_to :created_by, :class_name => 'User', optional: true
+  has_one :report
 end
