e284e2f90c9f6fa067a4803b9c8c245f5ae1c801
Danielid Ramirez <danielid132@gmail.com>
2021-10-07 20:22:40 -0300

    Modifying edit form and add JS

---
+++ app/assets/javascripts/devise.js
@@ -0,0 +1,8 @@
+document.addEventListener('turbolinks:load', function(){
+    
+})
+
+function blankPasswords(){
+    document.getElementById('user_password').value = '';
+    document.getElementById('user_password_confirmation').value = '';
+}
