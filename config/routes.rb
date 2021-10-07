2c38f3513bd3c544952a97443855419ad49546d6
Danielid Ramirez <danielid132@gmail.com>
2021-10-06 23:35:12 -0300

    Add: User controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :clients
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'users/registrations' }
   get 'home/index'
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
