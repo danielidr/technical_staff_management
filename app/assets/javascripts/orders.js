efb7fc9ba42a756d4abb3891441b38ace27d52de
Danielid Ramirez <danielid132@gmail.com>
2021-10-22 00:41:28 -0300

    Add google maps

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -78,3 +78,16 @@ function saveLocationForOrder(url) {
     })
 
 }
+
+function initMap(lat, lng) {
+    var myCoords = new google.maps.LatLng(lat, lng);
+    var mapOptions = {
+    center: myCoords,
+    zoom: 15
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var marker = new google.maps.Marker({
+        position: myCoords,
+        map: map
+    });
+}
