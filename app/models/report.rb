26dcacd9af5adf1d3caec881def2bcc10bae1f56
Danielid Ramirez <danielid132@gmail.com>
2021-10-25 20:12:05 -0300

    Modifying order status in reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,7 +10,7 @@ class Report < ApplicationRecord
 
     from = Email.new(email: 'danielid132@gmail.com')
     to = Email.new(email: client_email)
-    subject = "TechStaff - Reporte de orden #{self.id}"
+    subject = "TechStaff - Reporte de orden #{self.order.id}"
     content = Content.new(type: 'text/plain', value: "Estimado(a) #{client_name} #{client_lastname},\n\nSe envia reporte de la visita efectuada en su domicilio:\n\n\nPruebas Realizadas: #{self.tests_performed}\n\nComentarios: #{self.report_comments}\n\n\nSaludos Cordiales")
     mail = Mail.new(from, subject, to, content)    
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
@@ -19,5 +19,8 @@ class Report < ApplicationRecord
     puts response.status_code
     puts response.body
     puts response.headers
+    
+    # Cambia status a terminada
+    self.order.update(status: 3)
   end
 end
